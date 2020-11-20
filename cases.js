54a2b11523562182cb15a19b3366dfad6687cdb9
Matt G <thrasheremperor@gmail.com>
2020-11-20 18:14:08 -0300

    Subo productos files

---
+++ cases.js
@@ -0,0 +1,74 @@
+const { fstat } = require("fs"); 
+let process=require('process');
+let onlineStore=require('./functions.js');
+let commands=process.argv[2];
+
+switch(commands){
+    case 'list':
+        let products=onlineStore.read();
+        if(products.length===0){
+            console.log('Products are missing');
+        }else{
+            console.log("----------------------------");
+            console.log("        Product List        ");
+            console.log("----------------------------");
+            for(item in products){
+                console.log("ID: "+products[item].ID+" - Product: "+products[item].Product+" - Price: "+products[item].Price);
+            }
+        }
+        break;
+    case 'add':
+        let newID=process.argv[3];
+        let newProduct=process.argv[4];
+        let newPrice=process.argv[5];
+        onlineStore.add(newID,newProduct,newPrice);
+        return console.log("New item added!");
+    case 'filter':
+        let min=process.argv[3];
+        let max=process.argv[4];
+        let product=onlineStore.categorize(min,max);
+        if(product.length===0){
+            console.log("No products found!");
+        }else{
+            console.log("----------------------------");
+            console.log("    Categorized Products    ");
+            console.log("----------------------------");
+            for(item in product){
+                console.log("ID: "+product[item].ID+" - Product: "+product[item].Product+" - Price: "+product[item].Price);
+            }
+        }
+        break;
+    case 'modify':
+        let id = Number(process.argv[3]);
+        let priceChange = Number(process.argv[4]);    
+        if(id == undefined || priceChange == undefined){
+            console.log('----------------------------');
+            console.log("   ID must be valid");
+            console.log('----------------------------');
+        }
+        onlineStore.modify(id, priceChange);
+        console.log('--------------------------------');
+        console.log('   Product modified correctly');
+        console.log('--------------------------------');
+        break;
+    case 'erase':
+        let deleteId = Number(process.argv[3]);
+        if(deleteId == undefined){
+            console.log('-------------------------------------');
+            console.log('     ID not found');
+            console.log('-------------------------------------');
+        };
+        onlineStore.erase(deleteId);
+        console.log('--------------------------------');
+        console.log('     Product has been erased');
+        console.log('--------------------------------');
+        break;
+    case 'search':
+        let result=onlineStore.search(process.argv[3]);
+        result.forEach(function(item){
+            console.log(item);
+        });
+    case 'undo':
+        onlineStore.undo();
+        break;
+}
---
+++ functions.js
@@ -0,0 +1,61 @@
+let fs=require('fs');
+
+module.exports=onlineStore={
+    archive:'./productos.json',
+    read:function(){
+        let productList=fs.readFileSync(this.archive,'utf-8');
+        return JSON.parse(productList);
+    },
+    add:function(newId,newProduct,newPrice){
+        let newItem={
+            ID:newId,
+            Product:newProduct,
+            Price:newPrice
+        }
+        let lastProduct=this.read();
+        lastProduct.push(newItem);
+        this.save(lastProduct);
+    },
+    save:function(info){
+        let newJson=JSON.stringify(info);
+        fs.writeFileSync(this.archive,newJson,'utf-8');
+    },
+    erase:function(ID){
+        let list=this.read();
+        let eliminate=list.filter(function(Product){
+            return Product.ID!==ID;
+        });
+        let newList=JSON.stringify(eliminate);
+        fs.writeFileSync("./productos.json",newList,"utf-8");
+    },
+    search:function(find){
+        let productList=this.read();
+        let filteredItem=productList.filter(function(item){
+            return item.Product.toLowerCase().includes(find.toLowerCase())
+        });
+        return filteredItem;
+    },
+    categorize:function(min,max){
+        let list=this.read();
+        let result=list.filter(function(product){
+            return product.Price>=min&&product.Price<=max;
+        });
+        return result;
+    },
+    modify:function(ID, newPrice){ 
+        let list=this.read();
+        let productModified=list.filter((Product)=>{ 
+            if(Product.ID==ID){ 
+            Product.Price=newPrice 
+            }; 
+            return list 
+        });
+        let newProduct = JSON.stringify(productModified); 
+        fs.writeFileSync('./productos.json', newProduct, 'utf-8');
+    },
+    undo: function(){
+        let products=this.read();
+        products.pop();
+        this.save(products);
+    },
+}
